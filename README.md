7661d04b91f2536649c500d91364c158d7852a3b
Antonio <antonio.guimaraes@sou.inteli.edu.br>
2024-05-03 13:11:25 -0300

    add: exec file to install all dependencies and run ros2

--- README.md
+++ README.md
@@ -56,6 +56,26 @@ A outra solução, uma câmera endoscópica percorre o interior dos tubos. Esta 
 
 ## Inicialização
 
+Para inicializar a interface CLI, se seu terminal for .bash, execute o seguinte no seu terminal na respectiva ordem:
+
+```bash
+cd 2024-1B-T08-EC06-G04
+
+chmod +x exec_bash.sh
+
+./exec_bash.sh
+```
+
+Já, se seu terminal for .zsh, execute os seguinte códigos no terminal:
+
+```bash
+cd 2024-1B-T08-EC06-G04
+
+chmod +x exec_zsh.sh
+
+./exec_zsh.sh
+```
+
 ### Workspace ROS
 
 ---
---
+++ exec_zsh.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+cd src
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install typer inquirer
+
+cd project_ws
+
+TYPER_PATH=$(pip show typer | grep "Location:" | awk '{print $2}')
+
+if [ -z "$TYPER_PATH" ]; then
+    echo "Typer not found. Please make sure it is installed."
+    exit 1
+fi
+
+# Export the PYTHONPATH environment variable
+export PYTHONPATH="$PYTHONPATH:$TYPER_PATH"
+
+# Optionally: Echo PYTHONPATH to verify it
+echo "Updated PYTHONPATH: $PYTHONPATH"
+
+colcon build
+
+ros2 run cli_interface main
