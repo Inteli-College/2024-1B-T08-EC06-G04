a58207910c78230a574471a1fe68351706c75479
Antonio <antonio.guimaraes@sou.inteli.edu.br>
2024-05-08 14:17:28 -0300

    add: instruções readme

--- exec2_bash.sh
+++ exec2_bash.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Replace 'username' and 'hostname' with your SSH credentials
 SSH_USER="grupo4"
 SSH_HOST="10.128.0.9"
 
-# Open a new xterm window and run the SSH command to launch the ROS 2 node
 xterm -e "ssh $SSH_USER@$SSH_HOST 'source /opt/ros/foxy/setup.bash && ros2 launch turtlebot3_bringup robot.launch.py'" &
