d6e798e826fc279591ccba42609eaae4f825fdd7
Antonio <antonio.guimaraes@sou.inteli.edu.br>
2024-05-09 16:49:52 -0300

    fix: exec2 xterm

--- exec2_bash.sh
+++ exec2_bash.sh
@@ -3,8 +3,7 @@
 SSH_USER="grupo4"
 SSH_HOST="10.128.0.9"
 
-python3 -m venv venv
+sudo apt-get install -y xterm
 
-pip install xterm
-
+# Use xterm to open a new window and run the SSH command to launch ros2
 xterm -e "ssh $SSH_USER@$SSH_HOST 'source /opt/ros/foxy/setup.bash && ros2 launch turtlebot3_bringup robot.launch.py'" &
